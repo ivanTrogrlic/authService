ffb2f1dbb3eca0a65ff4e7e2d81566f881ffe707
Ivan Trogrlic <ivan.trogrlic@decode.agency>
2019-10-21 14:58:28 +0200

    Refactor FE to work with cookies

--- react-social/src/user/oauth2/OAuth2RedirectHandler.js
+++ react-social/src/user/oauth2/OAuth2RedirectHandler.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { ACCESS_TOKEN } from '../../constants';
-import { Redirect } from 'react-router-dom'
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom'
 
 class OAuth2RedirectHandler extends Component {
     getUrlParameter(name) {
@@ -11,24 +10,25 @@ class OAuth2RedirectHandler extends Component {
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     };
 
-    render() {        
-        const token = this.getUrlParameter('token');
+    render() {
         const error = this.getUrlParameter('error');
 
-        if(token) {
-            localStorage.setItem(ACCESS_TOKEN, token);
-            return <Redirect to={{
-                pathname: "/profile",
-                state: { from: this.props.location }
-            }}/>; 
-        } else {
+        if (error) {
             return <Redirect to={{
                 pathname: "/login",
-                state: { 
+                state: {
                     from: this.props.location,
-                    error: error 
+                    error: error
                 }
-            }}/>; 
+            }}/>;
+        } else {
+            return <Redirect to={{
+                pathname: "/profile",
+                state: {
+                    from: this.props.location,
+                    error: error
+                }
+            }}/>;
         }
     }
 }
--- react-social/src/util/APIUtils.js
+++ react-social/src/util/APIUtils.js
@@ -1,33 +1,27 @@
-import { API_BASE_URL, ACCESS_TOKEN } from '../constants';
+import {API_BASE_URL} from '../constants';
 
 const request = (options) => {
     const headers = new Headers({
         'Content-Type': 'application/json',
     })
-    
-    if(localStorage.getItem(ACCESS_TOKEN)) {
-        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
-    }
 
     const defaults = {headers: headers};
-    options = Object.assign({}, defaults, options);
+    options = {
+        ...defaults, ...options, credentials: "include"
+    };
 
     return fetch(options.url, options)
-    .then(response => 
-        response.json().then(json => {
-            if(!response.ok) {
-                return Promise.reject(json);
-            }
-            return json;
-        })
-    );
+        .then(response =>
+            response.json().then(json => {
+                if (!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        );
 };
 
 export function getCurrentUser() {
-    if(!localStorage.getItem(ACCESS_TOKEN)) {
-        return Promise.reject("No access token set.");
-    }
-
     return request({
         url: API_BASE_URL + "/user/me",
         method: 'GET'
